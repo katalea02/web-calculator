6d73b84d429ff8755ad20fadf9eb03ca0da45e72
Erigers <erigersmamoci@gmail.com>
2016-05-24 18:03:34 +0200

    fix

--- assets/js/numbers.js
+++ assets/js/numbers.js
@@ -1,12 +1,18 @@
-$(function(){
-    $counter = 0;
+$(document).ready(function(){
+    var counter = 0;
     $('.digits').on('click', function(){
-       if ($counter % 2 == 0) {
+       if (counter % 2 == 0) {
            $("input[type='number'][name='number1']").val($(this).html());
-           $counter++;
+           counter++;
        } else {
            $("input[type='number'][name='number2']").val($(this).html());
-           $counter++;
+           counter++;
        }
     });
-})();
+
+    var value = $('.result').text();
+    if(value.trim() == '' || value == null ) {
+        $('.result').hide();
+    }
+
+});
